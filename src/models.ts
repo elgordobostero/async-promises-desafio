b316f3bed3115d8baac5a8a05f2a6b4c82360aa0
Valentin Goroso <valegoroso21@gmail.com>
2024-10-24 21:21:40 -0300

    Mofique readFileSync y writeFileSync por readFile y writeFile

--- src/models.ts
+++ src/models.ts
@@ -9,8 +9,12 @@ class ContactsCollection {
   data: Contact[] = [];
   load() {
     // usar la version Async (readFile)
-    const json = jsonfile.readFileSync(__dirname + "/contacts.json");
-    this.data = json;
+    const promesa = jsonfile.readFile("./contacts.json");
+    promesa.then((json) => {
+      this.data = json;
+    });
+
+    return promesa;
   }
   getAll() {
     return this.data;
@@ -20,7 +24,7 @@ class ContactsCollection {
   }
   save() {
     // usar la version Async (writeFIle)
-    jsonfile.writeFileSync(__dirname + "/contacts.json", this.data);
+    jsonfile.writeFile("./contacts.json", this.data);
   }
   getOneById(id) {
     const encontrado = this.data.find((contacto) => {
